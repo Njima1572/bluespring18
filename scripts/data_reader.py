39cc28d2c9108aa6e05a573e233d5fd96c4c35fe
njima1572 <k_nakajima@coloradocollege.edu>
2020-08-22 19:48:01 +0900

    getting jr lines

--- scripts/data_reader.py
+++ scripts/data_reader.py
@@ -1,12 +1,19 @@
 import os
+import numpy as np
 import pandas as pd
 
 PROJ_ROOT = os.getenv("PROJ_ROOT")
 
 station_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "station20200619free.csv"))
-line_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "line20200619free.csv"))
 join_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "join20200619.csv"))
 
-print(station_df)
-print(line_df)
-print(join_df)
+# print(station_df["station_cd"])
+# print(join_df)
+
+def get_usable_line_cd():
+    line_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "line20200619free.csv"))
+    # Getting only JR lines from "line_name"
+    return line_df.loc[line_df["line_name"].str[:2] == "JR"]
+
+get_usable_line_cd()
+
