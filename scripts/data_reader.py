20f23c9a69d28cbf63819268af8296e70b7e0933
njima1572 <k_nakajima@coloradocollege.edu>
2020-08-22 20:01:27 +0900

    getting jr lines by company code

--- scripts/data_reader.py
+++ scripts/data_reader.py
@@ -4,16 +4,24 @@ import pandas as pd
 
 PROJ_ROOT = os.getenv("PROJ_ROOT")
 
-station_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "station20200619free.csv"))
-join_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "join20200619.csv"))
 
-# print(station_df["station_cd"])
-# print(join_df)
+class DataManager:
 
-def get_usable_line_cd():
-    line_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "line20200619free.csv"))
-    # Getting only JR lines from "line_name"
-    return line_df.loc[line_df["line_name"].str[:2] == "JR"]
+    def __init__(self):
+        self.station_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "station20200619free.csv"))
+        self.join_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "join20200619.csv"))
+        self.company_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "company20200619.csv"))
+        self.line_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "line20200619free.csv"))
 
-get_usable_line_cd()
+    def get_jr_sub_companies(self):
+        # 11 seems to be a JR rr_code
+        return self.company_df.loc[self.company_df["rr_cd"] == 11]
 
+    def get_jr_lines(self):
+        jr_sub_comapnies_cd = self.get_jr_sub_companies()["company_cd"]
+        return self.line_df.loc[self.line_df["company_cd"].isin(jr_sub_comapnies_cd)]
+
+dm = DataManager()
+
+
+print(dm.get_jr_lines())
