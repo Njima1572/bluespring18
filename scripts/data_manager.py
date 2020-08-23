f3772735f0651ca933ae92aca72987faedd7e144
kochi <kochi.nakajima@gmail.com>
2020-08-23 22:41:36 +0900

    modified company part

--- scripts/data_manager.py
+++ scripts/data_manager.py
@@ -4,14 +4,18 @@ import pandas as pd
 
 PROJ_ROOT = os.getenv("PROJ_ROOT")
 
+# Bullet train data is for paid people
 
 class DataManager:
-    def __init__(self, company_id=-1):
+    def __init__(self, company_id=-1, exclude_bullet=True):
         self.company_id = company_id
         self.station_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "station20200619free.csv"))
         self.join_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "join20200619.csv"))
         self.company_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "company20200619.csv"))
         self.line_df = pd.read_csv(os.path.join(PROJ_ROOT, "resources/data", "line20200619free.csv"))
+
+        self.station_df = self.station_df.sort_values("station_cd")
+        self.exclude_bullet = exclude_bullet
 
     # use rr_cd code
     def get_sub_companies(self):
@@ -21,17 +25,20 @@ class DataManager:
         return self.company_df.loc[self.company_df["rr_cd"] == self.company_id]
 
     # use rr_cd code to get lines
-    def get_lines(self, exclude_bullet=True):
+    def get_lines(self):
         sub_comapnies_cd = self.get_sub_companies()["company_cd"]
         lines_df = self.line_df.loc[self.line_df["company_cd"].isin(sub_comapnies_cd)]
-        if exclude_bullet:
+        if self.exclude_bullet:
             lines_df = lines_df.loc[self.line_df["line_cd"] > 10000]
         return lines_df
 
     # use rr_cd code to get stations
-    def get_stations(self, exclude_bullet=True):
-        lines = self.get_lines(exclude_bullet)
-        return self.station_df.loc[self.station_df["line_cd"].isin(lines["line_cd"])]
+    def get_stations(self):
+        return self.station_df
+
+    def get_joins(self):
+        lines = self.get_lines()
+        return self.join_df.loc[self.join_df["line_cd"].isin(lines["line_cd"])]
 
     def generate_line_cd_name_dict(self):
         lines_df = self.get_lines()
@@ -62,20 +69,39 @@ class DataManager:
         
         return station_dict
 
+    def generate_station_cd_g_cd_dict(self):
+        station_df = self.get_stations()
+        station_cd_g_cd_dict = {}
+
+        rows = station_df.itertuples()
+        for row in rows:
+            station_cd_g_cd_dict[row[1]] = row[2]
+
+        return station_cd_g_cd_dict
+
+    def get_station_name_from_g_cd(self, station_g_cd):
+        g_cd_dict = self.generate_station_g_cd_to_data_dict()
+        return g_cd_dict[station_g_cd][0]
+
+
     
     def set_company_id(self, company_id):
         self.company_id = company_id
 
+    def set_exclude_bullet(self, exclude_bullet):
+        self.exclude_bullet = exclude_bullet
+
 
 if __name__ == "__main__":
-    jr_company_id = 11
+    jr_company_id = 30
     dm = DataManager(jr_company_id)
     # print(dm.get_stations(jr_company_id))
     # print(dm.generate_line_cd_name_dict(jr_company_id))
     # print(dm.get_lines(jr_company_id))
     # print(dm.get_lines(jr_company_id, exclude_bullet=False))
     # print(dm.generate_station_to_station_df_line_group())
-    print(dm.generate_station_g_cd_to_data_dict())
+    # print(dm.generate_station_g_cd_to_data_dict())
+    # print(dm.get_joins_by_line_cd()[11342])
 
 
 
