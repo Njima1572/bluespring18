526129bf43139d9b2c2b9ab010e7385186977580
kochi <kochi.nakajima@gmail.com>
2020-08-23 13:56:53 +0900

    getting change of lines

--- scripts/data_manager.py
+++ scripts/data_manager.py
@@ -15,20 +15,49 @@ class DataManager:
     # use rr_cd code
     def get_sub_companies(self, company_id):
         # 11 seems to be a JR rr_code
+        if compnay_id < 0:
+            return self.compnay_df
         return self.company_df.loc[self.company_df["rr_cd"] == company_id]
 
     # use rr_cd code to get lines
-    def get_lines(self, company_id):
+    def get_lines(self, company_id, exclude_bullet=True):
         sub_comapnies_cd = self.get_sub_companies(company_id)["company_cd"]
-        return self.line_df.loc[self.line_df["company_cd"].isin(sub_comapnies_cd)]
-    
+        lines_df = self.line_df.loc[self.line_df["company_cd"].isin(sub_comapnies_cd)]
+        if exclude_bullet:
+            lines_df = lines_df.loc[self.line_df["line_cd"] > 10000]
+        return lines_df
     # use rr_cd code to get stations
-    def get_stations(self, company_id):
-        lines = self.get_lines(company_id)
+    def get_stations(self, company_id, exclude_bullet=True):
+        lines = self.get_lines(company_id, exclude_bullet)
         return self.station_df.loc[self.station_df["line_cd"].isin(lines["line_cd"])]
 
-dm = DataManager()
+    def generate_line_cd_name_dict(self, company_id):
+        lines_df = self.get_lines(company_id)
+        return lines_df.set_index('line_cd')['line_name_h'].to_dict()
 
-jr_company_id = 11
+    def genrate_station_to_station_df_line_group(self, company_id):
+        station_df = self.get_stations(company_id)
+        station_grouped = station_df.groupby("station_g_cd")
 
-print(dm.get_stations(jr_company_id))
+        # Trying to get stations with multiple change of lines
+        size_station_df = station_grouped.size().reset_index().rename(columns={"station_g_cd": "station_g_cd", 0:"count"})
+        size_station_df = size_station_df.loc[size_station_df["count"] > 1]
+
+        # Trying to make a groupby of each stations with changes
+        stations_with_changes_df = station_df.loc[station_df["station_g_cd"].isin(size_station_df["station_g_cd"])]
+        stations_with_changes_groupby = stations_with_changes_df.groupby("station_g_cd")
+
+        return stations_with_changes_groupby.groups
+
+
+if __name__ == "__main__":
+    dm = DataManager()
+    jr_company_id = 11
+    # print(dm.get_stations(jr_company_id))
+    # print(dm.generate_line_cd_name_dict(jr_company_id))
+    # print(dm.get_lines(jr_company_id))
+    # print(dm.get_lines(jr_company_id, exclude_bullet=False))
+    dm.generate_station_to_line_map(jr_company_id)
+
+
+
